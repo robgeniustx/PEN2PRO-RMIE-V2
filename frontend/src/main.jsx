627acfdbf41a2a1bf31850c223dbc4f218a15e4c
Pen2pro <frompen2pro@gmail.com>
2026-05-22 06:22:57 -0500

    Switch to HashRouter for Vercel routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import AppRoutes from './routes/AppRoutes'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppRoutes />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
